23ef8cade6a0cd65b7b83b425c1bbbc9c9257bf6
lucaspenna00 <lucaspenna000@gmail.com>
2021-06-27 16:43:11 -0300

    baixar dre improved

--- baixar_dre.py
+++ baixar_dre.py
@@ -1,10 +1,11 @@
 from equity_research.equity_research import EquityResearch
 from datetime import date
 
-ticker=input("Digite o Ticker desejado: ")
-ano=int(input("Digite o ano: "))
-mes=int(input("Digite o mes: "))
-dia=int(input("Digite o dia: "))
+ticker=input("[USER] Digite o Ticker desejado: ")
+ano=int(input("[USER] Digite o ano: "))
+mes=int(input("[USER] Digite o mes: "))
+dia=int(input("[USER] Digite o dia: "))
 
 eqr = EquityResearch()
 eqr.get_DRE(ticker, date(ano,mes,dia))
+print("[INFO] File generated in log file. Enjoy it!")
