0ce4c975039b691926bb161d26ab33cc843e7d74
lucaspenna00 <lucaspenna000@gmail.com>
2021-07-01 13:03:53 -0300

    run download improved

--- run_download_data.py
+++ run_download_data.py
@@ -21,16 +21,20 @@ def download():
     if space_in_gb < NECESSARY_SPACE: 
         raise Exception(f"You don't have enough space. The repository needs approximately 10G to work properly. You have just {space_in_gb} GB free in your hard disk.")
     else:
-        current_year = datetime.now().year
-        if args.date_init < 2011:
-            raise Exception("There is no CVM data before 2011.")
-        elif args.date_final > current_year:
-            raise Exception(f"There is no CVM data after {current_year}")
+        answer = input(f"[INFO] Space is not a problem. You have {space_in_gb} in your hard disk. Do you want to proceed? [Y/n]")
+        if answer == "Y" or answer == "y":
+            current_year = datetime.now().year
+            if args.date_init < 2011:
+                raise Exception("There is no CVM data before 2011.")
+            elif args.date_final > current_year:
+                raise Exception(f"There is no CVM data after {current_year}")
+            else:
+                print("[INFO] Downloading trimestral data from CVM...")
+                download_data.download_trimestral_data_from_cvm(args.date_init, args.date_final)
+                print("[INFO] Downloading anual data from CVM...")
+                download_data.download_anual_data_from_cvm(args.date_init, args.date_final)
         else:
-            print("[INFO] Downloading trimestral data from CVM...")
-            download_data.download_trimestral_data_from_cvm(args.date_init, args.date_final)
-            print("[INFO] Downloading anual data from CVM...")
-            download_data.download_anual_data_from_cvm(args.date_init, args.date_final)
+            print("[INFO] Operation canceled.")
 
 download()
 
